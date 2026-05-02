ed9394159915affa37993d5521217f2df8ac4180
Richard <meridian.supply.test@@gmail.com>
2026-05-01 22:05:47 -0700

    fix auth.ts session type

--- lib/auth.ts
+++ lib/auth.ts
@@ -25,7 +25,7 @@ export const authOptions: NextAuthOptions = {
       return token;
     },
     async session({ session, token }) {
-      session.accessToken = token.accessToken as string;
+      (session as any).accessToken = token.accessToken as string;s
       return session;
     },
   },
