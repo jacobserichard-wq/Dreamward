63b417a82c3b67137a7770949175fb5d1e5cf154
Richard <meridian.supply.test@@gmail.com>
2026-05-02 19:31:56 -0700

    fix: update Stripe API version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-12-18.acacia",
+  apiVersion: "2026-04-22.dahlia"
 });
 
 export const PLANS = {
